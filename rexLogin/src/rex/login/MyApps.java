8e18b73bca46d2305665baec171890c4ae536d82
John Nelson <jndbk1@hotmail.com>
2012-05-19 13:43:01 -0700

    Fixed up custom tabs

--- rexLogin/src/rex/login/MyApps.java
+++ rexLogin/src/rex/login/MyApps.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import rex.login.AppInfoHelper.AppDetails;
 import rex.login.AppInfoHelper.AppDetails.Times;
 
@@ -11,6 +12,7 @@ import android.R.drawable;
 import android.R.string;
 
 import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,7 +21,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TabHost;
+import android.widget.TabHost.TabContentFactory;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -86,30 +90,33 @@ public class MyApps extends Activity {
 				finish();
 			}
 		});
-	}
-	@Override
-	public void onWindowFocusChanged(boolean b){
-		mTabHost = (TabHost) findViewById(R.id.tabhost);
-		setupTab(new TextView(this), "Tab 1");
-		setupTab(new TextView(this), "Tab 2");
-		setupTab(new TextView(this), "Tab 3");
-	}
-	private void setupTab(final View view, final String tag) {
-		View tabview = createTabView(mTabHost.getContext(), tag);
-		TabSpec setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(new TabHost.TabContentFactory() {
+        setupTabHost();
+        //mTabHost.getTabWidget().setDividerDrawable(R.drawable.tab_divider);
 
-			public View createTabContent(String tag) {return view;}
-		});
-
-		mTabHost.addTab(setContent);
+        setupTab(new TextView(this), "Tab 1");
+        setupTab(new TextView(this), "Tab 2");
+        setupTab(new TextView(this), "Tab 3");
 	}
+    private void setupTabHost() {
+        mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup();
+    }
+    private void setupTab(final View view, final String tag) {
+        View tabview = createTabView(mTabHost.getContext(), tag);
 
-	private static View createTabView(final Context context, final String text) {
-		View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
-		TextView tv = (TextView) view.findViewById(R.id.tabsText);
-		tv.setText(text);
-		return view;
-	}	
+        TabSpec setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(new TabContentFactory() {
+            public View createTabContent(String tag) {return view;}
+        });
+        mTabHost.addTab(setContent);
+
+    }
+
+    private static View createTabView(final Context context, final String text) {
+        View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+        TextView tv = (TextView) view.findViewById(R.id.tabsText);
+        tv.setText(text);
+        return view;
+    }
 }
 
 
